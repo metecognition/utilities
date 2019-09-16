947fdaa1853c3ae753c5cb8370f2a75cada1b928
metecognition <35779829+metecognition@users.noreply.github.com>
2019-09-15 22:56:03 -0600

    Push 9/15
    Included the two parsing challenges from class

---
+++ Utilities/Assets/InputManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputManager : MonoBehaviour
+{
+    //UI Elements for text changing
+    public Text outputField;
+    public InputField inputField;
+    public Button runButton;
+
+     void Start()
+    {
+        outputField.text = "";
+    }
+
+    public void RunButtonPressed()
+    {
+        outputField.text = "Average Letters Per Word : " + Utilities.AverageWordLength(inputField.text).ToString();
+        //outputField.text = Utilities.ProcessText(inputField.text);
+    }
+}
---
+++ Utilities/Assets/Utilities.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utilities
+{
+
+    //Returns a string that says if each word is a word or a number
+    public static string ProcessText(string input)
+    {
+        var pieces = input.Split(" "[0]);
+
+
+        string output = "";
+        int i = 0;
+        float s;
+
+        while (i < pieces.Length)
+        {
+            if (float.TryParse(pieces[i], out s)) {
+                output = output + " number";
+            }
+            else
+            {
+                output = output + " word";
+            }
+            i++;
+        }
+
+        return output;
+    }
+
+    //Returns a float with the average number of letters per word in the input string
+    public static float AverageWordLength(string input)
+    {
+        var pieces = input.Split(" "[0]);
+        float numWords = pieces.Length;
+        float numLetters = 0;
+        float averageLetters = 0;
+        
+        for (int i = 0; i < numWords; i++)
+        {
+            numLetters += pieces[i].Length;
+        }
+        averageLetters = numLetters / numWords;
+        
+
+
+        return averageLetters;
+    }
+}
