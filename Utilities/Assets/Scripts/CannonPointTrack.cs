cf730243ade567c8d47556d30b196479245582d9
metecognition <35779829+metecognition@users.noreply.github.com>
2019-12-06 14:23:25 -0700

    More Random Scripts
    Added a 3d laser/line script, a two hand grab script, and reorganized some files for sanity's sake.

---
+++ Utilities/Assets/Scripts/CannonPointTrack.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OVR;
+
+public class CannonPointTrack : MonoBehaviour
+{
+    [Header("Aiming Variables")]
+    public GameObject leftController;
+    public GameObject rightController;
+    private Vector3 trackingPoint;
+    public float verticalOffset;
+
+    [Header("Grabbing Variables")]
+    public GameObject leftHandle;
+    public GameObject rightHandle;
+    public float grabDistance = 0.1f;
+    public float forceReleaseDistance = 0.5f;
+    private bool canGrabLeft, canGrabRight, isGrabbedLeft, isGrabbedRight = false;
+    public bool canFire = false;
+
+    [Header("Hand Positioning Variables")]
+    public GameObject leftHand;
+    public GameObject rightHand;
+
+
+    void Update(){
+
+        canFire = false;
+
+        //Check if can grab each handle
+        //Checks to see if player can grab handles based on distance
+        canGrabLeft = false;
+        canGrabRight = false;
+        if (Vector3.Distance(leftController.transform.position, leftHandle.transform.position) < grabDistance) {
+            canGrabLeft = true;
+            //Debug.Log("can grab left");
+        }
+        if (Vector3.Distance(rightController.transform.position, rightHandle.transform.position) < grabDistance) {
+            canGrabRight = true;
+            //Debug.Log("can grab right");
+        }
+
+
+        //Check grabbing
+        //Checks if player is attemping to grab handles, and if they can based on their current distance to the handles
+        if (OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger) > 0.9f) {
+            if (canGrabLeft & !isGrabbedLeft) {
+                isGrabbedLeft = true;
+            }
+        }
+        else {
+            isGrabbedLeft = false;
+        }
+        if (OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger) > 0.9f) {
+            if (canGrabRight & !isGrabbedRight) {
+                isGrabbedRight = true;
+            }
+        }
+        else {
+            isGrabbedRight = false;
+        }
+
+        //Debug.Log(isGrabbedLeft + " " + isGrabbedRight);
+
+        //Check force let go by distance
+        //If player has moved their hands too far from the handles, then they lose grip of handles
+        if (Vector3.Distance(leftController.transform.position, leftHandle.transform.position) > forceReleaseDistance) {
+            isGrabbedLeft = false;
+        }
+        if (Vector3.Distance(rightController.transform.position, rightHandle.transform.position) > forceReleaseDistance) {
+            isGrabbedRight = false;
+        }
+
+        //Moves tracking point
+        //If player is grabbing both handles, then the tracking point moves, rotating the cannon
+        if (isGrabbedLeft && isGrabbedRight) {
+            //Debug.Log("is grabbed");
+            canFire = true;
+            trackingPoint = (leftController.transform.position + rightController.transform.position) / 2;
+            trackingPoint.y -= verticalOffset;
+
+            transform.position = trackingPoint;
+        }
+
+        //Move hand objects to handles if the handle is grabbed
+        if (isGrabbedRight) {
+            rightHand.transform.position = rightHandle.transform.position;
+            rightHand.transform.rotation = rightHandle.transform.rotation;
+        }else {
+            rightHand.transform.position = rightController.transform.position;
+            rightHand.transform.rotation = rightController.transform.rotation;
+        }
+        if (isGrabbedLeft) {
+            leftHand.transform.position = leftHandle.transform.position;
+            leftHand.transform.rotation = leftHandle.transform.rotation;
+        }else {
+            leftHand.transform.position = leftController.transform.position;
+            leftHand.transform.rotation = leftController.transform.rotation;
+        }
+    }
+}
---
+++ Utilities/Assets/Scripts/Line3D.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+//-----------------------------------------------------------------------------------------------
+//This class creates a laser pointer line, or can be used to create a 3D line between two objects
+//-----------------------------------------------------------------------------------------------
+
+public class Line3D : MonoBehaviour
+{
+    //line should be a cylinder 1 unit tall, and 1 unity in radius
+    [Header("Line Settings")]
+    public Transform pointingObject;  //assign to hand, or whatever is shining the laser
+    public Transform pointingAt; //assign to raycast hit through code
+    public float radius; //recommend 0.02 for radius
+
+    //Note, pointEnd must be assigned regardless of whether it is used
+    [Header("Point Settings")]
+    public bool hasPoint = false;
+    public Transform pointEnd;
+    public float pointScale;  //if using default cube, I recommend something around 0.05;
+
+
+    
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //finds position between the hand and raycast hit
+        Vector3 newPos = Vector3.Lerp(pointingAt.position, pointingObject.position, 0.5f);
+        
+        //finds the required length to draw the lien
+        float length = Vector3.Distance(pointingObject.position, pointingAt.position);
+        Vector3 newScale = new Vector3(radius, radius, length) ;
+
+        //rotate to point at the raycast hit
+        transform.LookAt(pointingAt.position);
+
+        //assign transform variables
+        transform.position = newPos;
+        transform.localScale = newScale;
+
+        //if hasPoint is true, then move pointer to the raycast hit, and rotate it to face the hand
+        if (hasPoint)
+        {
+            pointEnd.position = pointingAt.position;
+            pointEnd.localScale = new Vector3(pointScale, pointScale, pointScale);
+            pointEnd.LookAt(pointingObject);
+        }
+    }
+}
